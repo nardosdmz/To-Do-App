12452158eeb79c40c727a905078a201b278a8157
nardosdmz <nardos0987@gmail.com>
2023-10-27 01:32:22 -0400

    change in axios

--- client/src/components/axios.jsx
+++ client/src/components/axios.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	// baseURL: "https://to-do-app-crud.onrender.com/api/",
-	baseURL: "http://localhost:5000/api",
+	baseURL: "https://to-do-app-crud.onrender.com/api/",
 });
 
 export default instance;
